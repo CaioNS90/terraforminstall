d147cd722bca5375735c9d6f8a0628f72fdba194
Caio Nunes <168440319+CaioNS90@users.noreply.github.com>
2024-07-13 12:44:09 -0300

    Update Script_terraform.sh

--- Script_terraform.sh
+++ Script_terraform.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Baixa e adiciona a chave GPG da HashiCorp ao keyring
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 # Adiciona o repositório da HashiCorp ao sources.list.d
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
 # Atualiza a lista de pacotes e instala o Terraform
-sudo apt update && sudo apt install terraform
+apt update && apt install terraform
